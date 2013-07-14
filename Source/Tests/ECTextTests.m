21aeb7308f49c362f678fe8704cb2223df5af4fb
Sam Deane <sam@elegantchaos.com>
2013-07-14 16:50:22 +0100

    Added some dummy tests.

---
+++ Source/Tests/ECTextTests.m
@@ -0,0 +1,28 @@
+// --------------------------------------------------------------------------
+//
+//  Copyright 2013 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import <ECUnitTests/ECUnitTests.h>
+
+@interface ECTextTests : ECTestCase
+{
+}
+
+@end
+
+@implementation ECTextTests
+
+- (void)testTest1
+{
+	STAssertTrue(YES, @"dummy test");
+}
+
+- (void)testTest2
+{
+	STAssertTrue(YES, @"dummy test");
+}
+
+@end
